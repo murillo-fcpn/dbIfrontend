48ecc5c70f02fa31ec7848300247e118f051d2c6
Israel Murillo FCPN <imurillof@fcpn.edu.bo>
2025-12-01 17:24:09 -0400

    first commit

---
+++ src/app/pages/pages.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Menu } from '../component/menu/menu';
+
+@Component({
+  selector: 'app-pages',
+  imports: [Menu],
+  templateUrl: './pages.html',
+  styleUrl: './pages.scss',
+})
+export class Pages {
+
+}
